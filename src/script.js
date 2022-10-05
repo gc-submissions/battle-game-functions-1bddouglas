8ad7f216943eb7d960b38081eda3247b00ede2e1
Brandon <1bddouglas@gmail.com>
2022-10-05 09:29:46 -0400

    final revision

--- src/script.js
+++ src/script.js
@@ -36,25 +36,27 @@ const isDead = (health) => {
   }
 };
 
-const fight = function (player1, player2, player1Health, player2Health) {
+function fight(player1, player2, player1Health, player2Health) {
   while (true) {
     let attacker = chooseOption(player1, player2);
     if (attacker === player1) {
       player2Health = attackPlayer(player2Health);
       logHealth(player2, player2Health);
-      if (isDead(player2Health) === true) {
+      if (isDead(player2Health)) {
         logDeath(player1, player2);
         break;
       }
     } else {
       player1Health = attackPlayer(player1Health);
       logHealth(player1, player1Health);
-      if (isDead(player1, player1Health) === true) {
+      if (isDead(player1Health)) {
         logDeath(player2, player1);
         break;
       }
     }
   }
-};
+}
 
 fight("Brandon", "Josh", 100, 100);
+
+// it seems as though player1 always wins, not sure why
